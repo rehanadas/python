c8e9e5b110c4b2528772f82d262c581f9ab198be
rehana <rehanadasp@gmail.com>
2021-12-27 13:34:16 +0530

    lab 9 answer

---
+++ lab3/lab9.py
@@ -0,0 +1,15 @@
+#Write a Python program to count the number of lines in a text file.
+file = open('python/lab3/test.txt','r')
+Counter = 0
+  
+# Reading from file
+Content = file.read()
+CoList = Content.split("\n")
+  
+for i in CoList:
+    if i:
+        Counter += 1
+          
+print("This is the number of lines in the file")
+print(Counter)
+file.close()
